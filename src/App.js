1ff0833102eae46ec2cf9d9c4d46cf1e88afaa8a
mirawlin <miranda.w.lin@gmail.com>
2019-12-06 09:21:11 +1100

    add edit and create button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Admin, Resource, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
-import {PostList} from './post'
+import {PostList, PostEdit, PostCreate} from './post'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} >
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
     <Resource name="users" list={UserList}/>
   </Admin>
 )
