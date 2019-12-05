29e98aa4b90e82072e8cc003221930dc4927237b
mirawlin <miranda.w.lin@gmail.com>
2019-12-06 09:05:26 +1100

    added posts page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { Admin, Resource} from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
+import {PostList} from './post'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} >
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    <Resource name="users" list={UserList}/>
   </Admin>
 )
 
